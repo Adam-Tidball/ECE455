124f390c978a53e552f5d1a97d0c50bb47abb81d
Adam-Tidball <76269064+Adam-Tidball@users.noreply.github.com>
2024-02-13 18:37:21 -0800

    Update main.c to recommended starting project

--- main.c
+++ main.c
@@ -1,3 +1,138 @@
+/*
+    FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
+    All rights reserved
+
+    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
+
+    ***************************************************************************
+    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
+    >>!   distribute a combined work that includes FreeRTOS without being   !<<
+    >>!   obliged to provide the source code for proprietary components     !<<
+    >>!   outside of the FreeRTOS kernel.                                   !<<
+    ***************************************************************************
+
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
+    link: http://www.freertos.org/a00114.html
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS provides completely free yet professionally developed,    *
+     *    robust, strictly quality controlled, supported, and cross          *
+     *    platform software that is more than just the market leader, it     *
+     *    is the industry's de facto standard.                               *
+     *                                                                       *
+     *    Help yourself get started quickly while simultaneously helping     *
+     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
+     *    tutorial book, reference manual, or both:                          *
+     *    http://www.FreeRTOS.org/Documentation                              *
+     *                                                                       *
+    ***************************************************************************
+
+    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
+    the FAQ page "My application does not run, what could be wwrong?".  Have you
+    defined configASSERT()?
+
+    http://www.FreeRTOS.org/support - In return for receiving this top quality
+    embedded software for free we request you assist our global community by
+    participating in the support forum.
+
+    http://www.FreeRTOS.org/training - Investing in training allows your team to
+    be as productive as possible as early as possible.  Now you can receive
+    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
+    Ltd, and the world's leading authority on the world's leading RTOS.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+    compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
+    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
+    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
+
+    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
+    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
+    licenses offer ticketed support, indemnification and commercial middleware.
+
+    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+    engineered and independently SIL3 certified version for use in safety and
+    mission critical applications that require provable dependability.
+
+    1 tab == 4 spaces!
+*/
+
+/*
+FreeRTOS is a market leading RTOS from Real Time Engineers Ltd. that supports
+31 architectures and receives 77500 downloads a year. It is professionally
+developed, strictly quality controlled, robust, supported, and free to use in
+commercial products without any requirement to expose your proprietary source
+code.
+
+This simple FreeRTOS demo does not make use of any IO ports, so will execute on
+any Cortex-M3 of Cortex-M4 hardware.  Look for TODO markers in the code for
+locations that may require tailoring to, for example, include a manufacturer
+specific header file.
+
+This is a starter project, so only a subset of the RTOS features are
+demonstrated.  Ample source comments are provided, along with web links to
+relevant pages on the http://www.FreeRTOS.org site.
+
+Here is a description of the project's functionality:
+
+The main() Function:
+main() creates the tasks and software timers described in this section, before
+starting the scheduler.
+
+The Queue Send Task:
+The queue send task is implemented by the prvQueueSendTask() function.
+The task uses the FreeRTOS vTaskDelayUntil() and xQueueSend() API functions to
+periodically send the number 100 on a queue.  The period is set to 200ms.  See
+the comments in the function for more details.
+http://www.freertos.org/vtaskdelayuntil.html
+http://www.freertos.org/a00117.html
+
+The Queue Receive Task:
+The queue receive task is implemented by the prvQueueReceiveTask() function.
+The task uses the FreeRTOS xQueueReceive() API function to receive values from
+a queue.  The values received are those sent by the queue send task.  The queue
+receive task increments the ulCountOfItemsReceivedOnQueue variable each time it
+receives the value 100.  Therefore, as values are sent to the queue every 200ms,
+the value of ulCountOfItemsReceivedOnQueue will increase by 5 every second.
+http://www.freertos.org/a00118.html
+
+An example software timer:
+A software timer is created with an auto reloading period of 1000ms.  The
+timer's callback function increments the ulCountOfTimerCallbackExecutions
+variable each time it is called.  Therefore the value of
+ulCountOfTimerCallbackExecutions will count seconds.
+http://www.freertos.org/RTOS-software-timer.html
+
+The FreeRTOS RTOS tick hook (or callback) function:
+The tick hook function executes in the context of the FreeRTOS tick interrupt.
+The function 'gives' a semaphore every 500th time it executes.  The semaphore
+is used to synchronise with the event semaphore task, which is described next.
+
+The event semaphore task:
+The event semaphore task uses the FreeRTOS xSemaphoreTake() API function to
+wait for the semaphore that is given by the RTOS tick hook function.  The task
+increments the ulCountOfReceivedSemaphores variable each time the semaphore is
+received.  As the semaphore is given every 500ms (assuming a tick frequency of
+1KHz), the value of ulCountOfReceivedSemaphores will increase by 2 each second.
+
+The idle hook (or callback) function:
+The idle hook function queries the amount of free FreeRTOS heap space available.
+See vApplicationIdleHook().
+
+The malloc failed and stack overflow hook (or callback) functions:
+These two hook functions are provided as examples, but do not contain any
+functionality.
+*/
 
 /* Standard includes. */
 #include <stdint.h>
@@ -14,6 +149,17 @@
 
 
 /*-----------------------------------------------------------*/
+#define mainQUEUE_LENGTH 100
+
+#define amber  	0
+#define green  	1
+#define red  	2
+#define blue  	3
+
+#define amber_led	LED3
+#define green_led	LED4
+#define red_led		LED5
+#define blue_led	LED6
 
 
 /*
@@ -27,18 +173,12 @@ static void prvSetupHardware( void );
  * this file.
  */
 static void Manager_Task( void *pvParameters );
+static void Blue_LED_Controller_Task( void *pvParameters );
+static void Green_LED_Controller_Task( void *pvParameters );
+static void Red_LED_Controller_Task( void *pvParameters );
+static void Amber_LED_Controller_Task( void *pvParameters );
 
 xQueueHandle xQueue_handle = 0;
-
-void led_init( void );
-void led_data( unsigned char data );
-void adc_init( void );
-void status_toggle( void );
-uint16_t adc_convert( void );
-
-void Delay( void );
-
-unsigned char digits[10] = { 0x3f, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
 
 
 /*-----------------------------------------------------------*/
@@ -46,15 +186,30 @@ unsigned char digits[10] = { 0x3f, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7
 int main(void)
 {
 
-	adc_init();
-	led_init();
+	/* Initialize LEDs */
+	STM_EVAL_LEDInit(amber_led);
+	STM_EVAL_LEDInit(green_led);
+	STM_EVAL_LEDInit(red_led);
+	STM_EVAL_LEDInit(blue_led);
 
 	/* Configure the system ready to run the demo.  The clock configuration
 	can be done here if it was not done before main() was called. */
 	prvSetupHardware();
 
 
+	/* Create the queue used by the queue send and queue receive tasks.
+	http://www.freertos.org/a00116.html */
+	xQueue_handle = xQueueCreate( 	mainQUEUE_LENGTH,		/* The number of items the queue can hold. */
+							sizeof( uint16_t ) );	/* The size of each item the queue holds. */
+
+	/* Add to the registry, for the benefit of kernel aware debugging. */
+	vQueueAddToRegistry( xQueue_handle, "MainQueue" );
+
 	xTaskCreate( Manager_Task, "Manager", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate( Blue_LED_Controller_Task, "Blue_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate( Red_LED_Controller_Task, "Red_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate( Green_LED_Controller_Task, "Green_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate( Amber_LED_Controller_Task, "Amber_LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
@@ -67,50 +222,142 @@ int main(void)
 
 static void Manager_Task( void *pvParameters )
 {
-//
-// Display mode range 1 to 3
-//
-	uint16_t mode = 1;
+	uint16_t tx_data = amber;
 
-	uint16_t counter = 0;
-	uint16_t delay = 0;
-
-	uint16_t adc_data = 0;
 
 	while(1)
 	{
-		adc_data = adc_convert() / 409;
-		if ( adc_data >= 10 )
-		{
-			adc_data = 9;
-		}
 
+		if(tx_data == amber)
+			STM_EVAL_LEDOn(amber_led);
+		if(tx_data == green)
+			STM_EVAL_LEDOn(green_led);
+		if(tx_data == red)
+			STM_EVAL_LEDOn(red_led);
+		if(tx_data == blue)
+			STM_EVAL_LEDOn(blue_led);
 
-		if ( mode == 1 )
+		if( xQueueSend(xQueue_handle,&tx_data,1000))
 		{
-			status_toggle();
-			delay = 250;
-			led_data( digits[counter] );
-		}
-		else if ( mode == 2 )
-		{
-			delay = 250;
-			led_data( digits[adc_data] );
+			printf("Manager: %u ON!\n", tx_data);
+			if(++tx_data == 4)
+				tx_data = 0;
+			vTaskDelay(1000);
 		}
 		else
 		{
-			delay = (100*adc_data) + 100;
-			led_data( digits[counter] );
+			printf("Manager Failed!\n");
 		}
+	}
+}
 
-		counter = counter + 1;
-		if ( counter >= 10 )
+/*-----------------------------------------------------------*/
+
+static void Blue_LED_Controller_Task( void *pvParameters )
+{
+	uint16_t rx_data;
+	while(1)
+	{
+		if(xQueueReceive(xQueue_handle, &rx_data, 500))
 		{
-			counter = 0;
+			if(rx_data == blue)
+			{
+				vTaskDelay(250);
+				STM_EVAL_LEDOff(blue_led);
+				printf("Blue Off.\n");
+			}
+			else
+			{
+				if( xQueueSend(xQueue_handle,&rx_data,1000))
+					{
+						printf("BlueTask GRP (%u).\n", rx_data); // Got wwrong Package
+						vTaskDelay(500);
+					}
+			}
 		}
+	}
+}
 
 
-		vTaskDelay( delay );
+/*-----------------------------------------------------------*/
+
+static void Green_LED_Controller_Task( void *pvParameters )
+{
+	uint16_t rx_data;
+	while(1)
+	{
+		if(xQueueReceive(xQueue_handle, &rx_data, 500))
+		{
+			if(rx_data == green)
+			{
+				vTaskDelay(250);
+				STM_EVAL_LEDOff(green_led);
+				printf("Green Off.\n");
+			}
+			else
+			{
+				if( xQueueSend(xQueue_handle,&rx_data,1000))
+					{
+						printf("GreenTask GRP (%u).\n", rx_data); // Got wrong Package
+						vTaskDelay(500);
+					}
+			}
+		}
+	}
+}
+
+/*-----------------------------------------------------------*/
+
+static void Red_LED_Controller_Task( void *pvParameters )
+{
+	uint16_t rx_data;
+	while(1)
+	{
+		if(xQueueReceive(xQueue_handle, &rx_data, 500))
+		{
+			if(rx_data == red)
+			{
+				vTaskDelay(250);
+				STM_EVAL_LEDOff(red_led);
+				printf("Red off.\n");
+			}
+			else
+			{
+				if( xQueueSend(xQueue_handle,&rx_data,1000))
+					{
+						printf("RedTask GRP (%u).\n", rx_data); // Got wrong Package
+						vTaskDelay(500);
+					}
+			}
+		}
+	}
+}
+
+
+/*-----------------------------------------------------------*/
+
+static void Amber_LED_Controller_Task( void *pvParameters )
+{
+	uint16_t rx_data;
+	while(1)
+	{
+		if(xQueueReceive(xQueue_handle, &rx_data, 500))
+		{
+			if(rx_data == amber)
+			{
+				vTaskDelay(250);
+				STM_EVAL_LEDOff(amber_led);
+				printf("Amber Off.\n");
+			}
+			else
+			{
+				if( xQueueSend(xQueue_handle,&rx_data,1000))
+					{
+						printf("AmberTask GRP (%u).\n", rx_data); // Got wrong Package
+						vTaskDelay(500);
+					}
+			}
+		}
 	}
 }
 
